dfae05d467a6de79c10de896eb7667614342cf0c
fabiantfandi-svg <fabiantfandi@gmail.com>
2026-09-18 07:25:35 -0500

    log

--- Frontend/src/app/app.config.ts
+++ Frontend/src/app/app.config.ts
@@ -1,9 +1,12 @@
 import { ApplicationConfig } from '@angular/core';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { routes } from './app.routes';
+import { authInterceptor } from './services/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideHttpClient(withInterceptors([authInterceptor])),
     provideRouter(
       routes,
       withInMemoryScrolling({
